ee47ecf4478e199c67c99bd04cd93d73ae62d4d4
Mrk0p <sachinwele23@gmail.com>
2023-01-06 21:42:40 +0530

    ADDED:functional programs

---
+++ src/com/bridgelabz/functional_programs/Array_2D.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.functional_programs;
+
+import java.util.Scanner;
+
+public class Array_2D {
+
+    public static void main(String[] args) {
+        int num[][] = new int[3][3];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Matrix values: ");
+
+        for (int i=0;i<3;i++){
+            for (int j=3;j<3;j++){
+                num[i][j] = scanner.nextInt();
+            }
+            System.out.println();
+        }
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                System.out.print(num[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
---
+++ src/com/bridgelabz/functional_programs/WindChill.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.functional_programs;
+
+import java.util.Scanner;
+
+public class WindChill {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the values of temperature in Fahrenheit:");
+        double t = scanner.nextDouble();
+
+        System.out.println("Enter the value of wind speed in miles per hour");
+        double v = scanner.nextDouble();
+
+
+        if (t < 50 && v > 3 && v < 120)
+        {
+            double windchill = 35.74 + 0.6215 * t + (0.4275 * t - 35.75) * (Math.pow(v, 0.16));
+            System.out.println("Wind chill factor is:" + windchill);
+        }
+        else
+            System.out.println("Out of range values!");
+    }
+}
